c9210eeee6b84202f09d08d08053b379672fbd33
kubadusza <jacob.dusza@gmail.com>
2023-10-14 22:45:06 +0200

    fixed access key access

--- modules/setup.py
+++ modules/setup.py
@@ -66,7 +66,7 @@ def grant_access():
     query_access_key = query_dict.get("access_key", None)
 
     if query_access_key:
-        st.session_state.access_key = query_access_key
+        st.session_state.access_key = query_access_key[0]
 
     if (st.experimental_user.email in allowed_emails) or (
             st.session_state.access_key in allowed_access_keys) or REMOVE_RESTRICTIONS:
